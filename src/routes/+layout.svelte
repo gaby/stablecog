cc7ae292cfeaa4ae1aa3d99b8434a030c48a0d1b
yekta <ngyekta@gmail.com>
2023-02-16 20:37:10 +0300

    Back online

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -123,15 +123,15 @@
 		? `min-height: ${innerHeight}px`
 		: ''}"
 >
-	<div class="w-full flex-1 flex flex-col justify-center items-center min-h-screen">
+	<!-- <div class="w-full flex-1 flex flex-col justify-center items-center min-h-screen">
 		<div class="flex flex-col items-center justify-center my-auto max-w-sm text-center px-6 py-6">
 			<Logo class="w-16 h-16" />
 			<p class="mt-5 font-bold text-2xl">Under maintenance</p>
 			<p class="mt-2 text-c-on-bg/60">We're migrating our database, we'll be back in 1-2 hours.</p>
 			<div class="w-full h-[4vh]" />
 		</div>
-	</div>
-	<!-- <Navbar />
+	</div> -->
+	<Navbar />
 	<main class="w-full flex-1 flex flex-col relative break-words">
 		<slot />
 	</main>
@@ -143,5 +143,5 @@
 	<div
 		id="tooltip-container"
 		class="absolute overflow-x-hidden left-0 top-0 w-full h-full pointer-events-none"
-	/> -->
+	/>
 </div>
