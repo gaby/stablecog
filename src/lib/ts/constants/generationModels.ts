b7ed7e7ddd8d8d18027f1eed3ec5d6734aef1b90
yekta <ngyekta@gmail.com>
2026-02-24 04:05:03 +0300

    Remove low-use models

--- src/lib/ts/constants/generationModels.ts
+++ src/lib/ts/constants/generationModels.ts
@@ -26,11 +26,11 @@ export const availableGenerationModelIds = [
 	'0a99668b-45bd-4f7e-aa9c-f9aaa41ef13b',
 	'986d447d-c38b-4218-a2c8-6e0b691f47ec',
 	'9fa49c00-109d-430f-9ddd-449f02e2c71a',
-	'4e54440f-ee17-4712-b4b6-0671b94d685d',
-	'8002bc51-7260-468f-8840-cf1e6dbe3f8a',
+	'4e54440f-ee17-4712-b4b6-0671b94d685d'
+	/* '8002bc51-7260-468f-8840-cf1e6dbe3f8a',
 	'b6c1372f-31a7-457c-907c-d292a6ffef97',
 	'fc06f6ab-ed14-4186-a7c0-aaec288d4f38',
-	'f7f3d973-ac6f-4a7a-9db8-e89e4fba03a9'
+	'f7f3d973-ac6f-4a7a-9db8-e89e4fba03a9' */
 ] as const;
 
 export const AvailableGenerationModelIdSchema = z.enum(availableGenerationModelIds);
@@ -125,11 +125,11 @@ export const modelIdToDisplayName = derived<
 		'0a99668b-45bd-4f7e-aa9c-f9aaa41ef13b': 'FLUX.1',
 		'986d447d-c38b-4218-a2c8-6e0b691f47ec': 'Stable Diffusion 3',
 		'9fa49c00-109d-430f-9ddd-449f02e2c71a': 'Kandinsky 2.2',
-		'4e54440f-ee17-4712-b4b6-0671b94d685d': 'SSD-1B',
-		'8002bc51-7260-468f-8840-cf1e6dbe3f8a': 'SDXL',
+		'4e54440f-ee17-4712-b4b6-0671b94d685d': 'SSD-1B'
+		/* '8002bc51-7260-468f-8840-cf1e6dbe3f8a': 'SDXL',
 		'b6c1372f-31a7-457c-907c-d292a6ffef97': 'Luna Diffusion',
 		'fc06f6ab-ed14-4186-a7c0-aaec288d4f38': '22h Diffusion',
-		'f7f3d973-ac6f-4a7a-9db8-e89e4fba03a9': 'Waifu Diffusion'
+		'f7f3d973-ac6f-4a7a-9db8-e89e4fba03a9': 'Waifu Diffusion' */
 	};
 	for (const modelId in generationModels) {
 		obj[modelId as TAvailableGenerationModelId] =
@@ -158,8 +158,8 @@ export const availableModelIdDropdownItems = derived(
 			{
 				label: $modelIdToDisplayName['4e54440f-ee17-4712-b4b6-0671b94d685d'],
 				value: '4e54440f-ee17-4712-b4b6-0671b94d685d'
-			},
-			{
+			}
+			/* {
 				label: $modelIdToDisplayName['8002bc51-7260-468f-8840-cf1e6dbe3f8a'],
 				value: '8002bc51-7260-468f-8840-cf1e6dbe3f8a'
 			},
@@ -174,7 +174,7 @@ export const availableModelIdDropdownItems = derived(
 			{
 				label: $modelIdToDisplayName['f7f3d973-ac6f-4a7a-9db8-e89e4fba03a9'],
 				value: 'f7f3d973-ac6f-4a7a-9db8-e89e4fba03a9'
-			}
+			} */
 		];
 		return items;
 	}
